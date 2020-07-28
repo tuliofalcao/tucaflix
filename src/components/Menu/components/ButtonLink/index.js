c43b76c28fcae7cf6cb039114dce7b35da631941
Túlio Falcão <falcao.tulio@gmail.com>
2020-07-28 17:00:19 -0300

    Adding styled-components

--- src/components/Menu/components/ButtonLink/index.js
+++ src/components/Menu/components/ButtonLink/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ButtonLink(props) {
     return (
         <a href={props.href} className={props.className}>
-            Novo Vídeo
+            {props.children}
         </a>
     );
 }
