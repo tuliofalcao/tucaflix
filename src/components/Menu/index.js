61ecaa34bff4fc78eecb29b21af38bae97047e6e
Túlio Falcão <falcao.tulio@gmail.com>
2020-07-30 01:02:57 -0300

    Carrosel

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../assets/img/logo.png';
+import Logo from '../../assets/img/logo.png';
 import './Menu.css';
 import Button from '../Button';
-
 // import ButtonLink from './components/ButtonLink';
 
 function Menu() {
-    return (
-        <nav className="Menu">
-            <Link to="/">
-                <img className="logo" src={logo} alt="TucaFlix Logo" />
-            </Link>
-            <Button as={Link} className="ButtonLink" to="/cadastro/video">
-                Novo Vídeo
-            </Button>
-        </nav>
-    );
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </Link>
+
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './pages/Home';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video';
 import CadastroCategoria from './pages/cadastro/Categoria';
+
+// Desafio master blaster na descrição
+// Colocar um jogo nessa página: https://www.youtube.com/watch?v=jOAU81jdi-c :)
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
   <BrowserRouter>
@@ -13,7 +17,7 @@ ReactDOM.render(
       <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
       <Route path="/cadastro/categoria" component={CadastroCategoria} />
-      <Route  component={() => ( <div>Página 404</div>)} />
+      <Route component={Pagina404} />
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
